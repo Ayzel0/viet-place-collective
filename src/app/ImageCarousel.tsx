de7efc2b17e41ca31d30a7dd5f13fb38a0437823
ayzel <andrewzlee@gmail.com>
2024-04-22 10:37:41 -0400

    fix transition

--- src/app/ImageCarousel.tsx
+++ src/app/ImageCarousel.tsx
@@ -12,7 +12,7 @@ const ImageCarousel = () => {
   return (
     <div className='flex flex-row gap-8'>
       <div 
-        className={`flex flex-row relative w-[15%] transition-[width] ease-in-out h-[400px] ${expandedImageIndex === 0 && 'w-[55%]'}`}
+        className={`flex flex-row relative w-[15%] transition-[width] ease-linear h-[400px] ${expandedImageIndex === 0 && 'w-[55%]'}`}
         onMouseOver={() => setExpandedImageIndex(0)}
       >
         <div className={`relative transition-[width] ${expandedImageIndex === 0 ? 'w-[60%]' : 'w-full'} `}>
@@ -23,7 +23,7 @@ const ImageCarousel = () => {
             className='object-cover'
           />
         </div>
-        <div className={`transition-[width] flex flex-col items-center justify-center bg-medium-jade overflow-x-hidden ${expandedImageIndex === 0 ? 'w-[40%]' : 'w-0'}`}>
+        <div className={`transition-[width] ease-linear flex flex-col items-center justify-center bg-medium-jade overflow-x-hidden ${expandedImageIndex === 0 ? 'w-[40%]' : 'w-0'}`}>
           {language === 'english' ?
             <h2 className='text-white px-2 text-nowrap text-2xl text-center'>About the<br></br>Small Area Plan</h2>
             :
@@ -37,38 +37,86 @@ const ImageCarousel = () => {
           </button>
         </div>
       </div>
+
       <div 
-        className={`relative w-[15%] transition-[width] ease-in-out h-[400px] ${expandedImageIndex === 1 && 'w-[55%]'}`}
+        className={`flex flex-row relative w-[15%] transition-[width] ease-linear h-[400px] ${expandedImageIndex === 1 && 'w-[55%]'}`}
         onMouseOver={() => setExpandedImageIndex(1)}
       >
-        <Image 
-          src={'/edenCenterInterior.jpg'}
-          alt={'eden center interior'}
-          fill
-          className='object-cover'
-        />
+        <div className={`relative transition-[width] ${expandedImageIndex === 1 ? 'w-[60%]' : 'w-full'} `}>
+          <Image 
+            src={'/edenCenterInterior.jpg'}
+            alt={'eden center interior'}
+            fill
+            className='object-cover'
+          />
+        </div>
+        <div className={`transition-[width] ease-linear flex flex-col items-center justify-center bg-medium-jade overflow-x-hidden ${expandedImageIndex === 1 ? 'w-[40%]' : 'w-0'}`}>
+          {language === 'english' ?
+            <h2 className='text-white px-2 text-nowrap text-2xl text-center'>About the<br></br>Small Area Plan</h2>
+            :
+            <h2 className='text-white px-2 text-nowrap text-2xl text-center'>Về kế hoạch<br></br>diện tích nhỏ</h2>
+          }
+          <button className='bg-charcoal rounded-md mt-2 w-[50%]'>
+            <h3 className={`text-white py-2 text-lg ${fraunces.className}`}>{language === 'english' ? 'Read' : 'Đọc'}</h3>
+          </button>
+          <button className='bg-charcoal rounded-md mt-2 w-[50%]'>
+            <h3 className={`text-white py-2 text-lg ${fraunces.className}`}>{language === 'english' ? 'Demands' : 'Yêu cầu'}</h3>
+          </button>
+        </div>
       </div>
+      
       <div 
-        className={`relative w-[15%] transition-[width] ease-in-out h-[400px] ${expandedImageIndex === 2 && 'w-[55%]'}`}
+        className={`flex flex-row relative w-[15%] transition-[width] ease-linear h-[400px] ${expandedImageIndex === 2 && 'w-[55%]'}`}
         onMouseOver={() => setExpandedImageIndex(2)}
       >
-        <Image 
-          src={'/edenCenterInterior.jpg'}
-          alt={'eden center interior'}
-          fill
-          className='object-cover'
-        />
+        <div className={`relative transition-[width] ${expandedImageIndex === 2 ? 'w-[60%]' : 'w-full'} `}>
+          <Image 
+            src={'/edenCenterInterior.jpg'}
+            alt={'eden center interior'}
+            fill
+            className='object-cover'
+          />
+        </div>
+        <div className={`transition-[width] ease-linear flex flex-col items-center justify-center bg-medium-jade overflow-x-hidden ${expandedImageIndex === 2 ? 'w-[40%]' : 'w-0'}`}>
+          {language === 'english' ?
+            <h2 className='text-white px-2 text-nowrap text-2xl text-center'>About the<br></br>Small Area Plan</h2>
+            :
+            <h2 className='text-white px-2 text-nowrap text-2xl text-center'>Về kế hoạch<br></br>diện tích nhỏ</h2>
+          }
+          <button className='bg-charcoal rounded-md mt-2 w-[50%]'>
+            <h3 className={`text-white py-2 text-lg ${fraunces.className}`}>{language === 'english' ? 'Read' : 'Đọc'}</h3>
+          </button>
+          <button className='bg-charcoal rounded-md mt-2 w-[50%]'>
+            <h3 className={`text-white py-2 text-lg ${fraunces.className}`}>{language === 'english' ? 'Demands' : 'Yêu cầu'}</h3>
+          </button>
+        </div>
       </div>
+
       <div 
-        className={`relative w-[15%] transition-[width] ease-in-out h-[400px] ${expandedImageIndex === 3 && 'w-[55%]'}`}
+        className={`flex flex-row relative w-[15%] transition-[width] ease-linear h-[400px] ${expandedImageIndex === 3 && 'w-[55%]'}`}
         onMouseOver={() => setExpandedImageIndex(3)}
       >
-        <Image 
-          src={'/edenCenterInterior.jpg'}
-          alt={'eden center interior'}
-          fill
-          className='object-cover'
-        />
+        <div className={`relative transition-[width] ${expandedImageIndex === 3 ? 'w-[60%]' : 'w-full'} `}>
+          <Image 
+            src={'/edenCenterInterior.jpg'}
+            alt={'eden center interior'}
+            fill
+            className='object-cover'
+          />
+        </div>
+        <div className={`transition-[width] ease-linear flex flex-col items-center justify-center bg-medium-jade overflow-x-hidden ${expandedImageIndex === 3 ? 'w-[40%]' : 'w-0'}`}>
+          {language === 'english' ?
+            <h2 className='text-white px-2 text-nowrap text-2xl text-center'>About the<br></br>Small Area Plan</h2>
+            :
+            <h2 className='text-white px-2 text-nowrap text-2xl text-center'>Về kế hoạch<br></br>diện tích nhỏ</h2>
+          }
+          <button className='bg-charcoal rounded-md mt-2 w-[50%]'>
+            <h3 className={`text-white py-2 text-lg ${fraunces.className}`}>{language === 'english' ? 'Read' : 'Đọc'}</h3>
+          </button>
+          <button className='bg-charcoal rounded-md mt-2 w-[50%]'>
+            <h3 className={`text-white py-2 text-lg ${fraunces.className}`}>{language === 'english' ? 'Demands' : 'Yêu cầu'}</h3>
+          </button>
+        </div>
       </div>
     </div>
   )
