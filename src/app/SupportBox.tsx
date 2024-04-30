9cb9013f3e322af725c0d1b4ad65911e20f509bd
ayzel <andrewzlee@gmail.com>
2024-04-30 10:12:06 -0400

    hello

--- src/app/SupportBox.tsx
+++ src/app/SupportBox.tsx
@@ -20,9 +20,9 @@ const SupportBox = () => {
   return (
     <div className='grid lg:grid-cols-3 gap-8 rounded-xl my-4'>
       {Object.keys(supportBoxContent).map(boxName => (
-        <div key={boxName} className='bg-mint p-4 rounded-xl'>
+        <div key={boxName} className='bg-mint p-4 rounded-xl flex flex-col'>
           <h2 className='text-dark-jade text-2xl font-semibold'>{supportBoxContent[boxName].title}</h2>
-          <p className='text-charcoal my-2'>{supportBoxContent[boxName].text}</p>
+          <p className='text-charcoal my-2 grow'>{supportBoxContent[boxName].text}</p>
           <div 
             className={`cursor-pointer bg-charcoal text-white p-2 text-center rounded-full text-lg hover:brightness-125 ${fraunces.className}`}
             onClick={() => {window.location.href = supportBoxContent[boxName].link}}
