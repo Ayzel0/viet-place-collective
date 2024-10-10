8a029efaebd88fce5144da8247b3e9fc31800e5a
ayzel <andrewzlee@gmail.com>
2024-10-09 19:43:04 -0500

    center contact page text

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -14,8 +14,8 @@ const Page = () => {
         <h1 className='z-20 text-[4rem] text-shadow-md font-bold text-white absolute top-[50%] -translate-y-[50%] left-[50%] -translate-x-[50%]'>Connect With Us</h1>
       </div>
       <div className='mx-[5%] lg:mx-[20%] xl:mx-[25%] my-10'>
-        <h2 className='text-white mt-8 text-2xl'>We&apos;re most active on <a href="https://www.facebook.com/avietplace/" target='_blank' className='underline text-mint'>Facebook</a> and <a href="https://www.instagram.com/avietplace" target='_blank' className='underline text-mint'>Instagram</a>.</h2>
-        <h2 className='text-white mt-8 text-2xl'>Reach us via email at hello@avietplace.org.</h2>
+        <h2 className='text-white mt-8 text-2xl text-center'>We&apos;re most active on <a href="https://www.facebook.com/avietplace/" target='_blank' className='underline text-mint'>Facebook</a> and <a href="https://www.instagram.com/avietplace" target='_blank' className='underline text-mint'>Instagram</a>.</h2>
+        <h2 className='text-white mt-8 text-2xl text-center'>Reach us via email at hello@avietplace.org.</h2>
       </div>
     </div>
   );
